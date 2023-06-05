90e7aab2407393657b5357795d6a3a37f5ca223a
Parker Hodge <iparkerhodge@gmail.com>
2023-06-04 23:10:36 -0500

    forgot to check in routes

--- config/routes.rb
+++ config/routes.rb
@@ -2,8 +2,8 @@ Rails.application.routes.draw do
   post 'sign_up', to: 'users#create'
   post 'login', to: 'users#index'
 
-  get 'charge/index'
-  post 'charge', to: 'charge#create'
+  get 'receipt/index'
+  post 'receipt', to: 'receipt#create'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
